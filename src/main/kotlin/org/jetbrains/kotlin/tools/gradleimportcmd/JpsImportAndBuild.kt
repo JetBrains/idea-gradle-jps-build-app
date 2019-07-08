be24eb00ac15adca611a6f72d0893974d96e639d
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2019-07-08 03:12:38 +0300

    skip index initialization

--- src/main/kotlin/org/jetbrains/kotlin/tools/gradleimportcmd/JpsImportAndBuild.kt
+++ src/main/kotlin/org/jetbrains/kotlin/tools/gradleimportcmd/JpsImportAndBuild.kt
@@ -1,12 +1,10 @@
 package org.jetbrains.kotlin.tools.gradleimportcmd
 
-import org.jetbrains.kotlin.tools.testutils.TestSuite
-import org.jetbrains.kotlin.tools.testutils.buildProject
-import org.jetbrains.kotlin.tools.testutils.importProject
-import org.jetbrains.kotlin.tools.testutils.setDelegationMode
+import org.jetbrains.kotlin.tools.testutils.*
 
 class JpsImportAndBuild : ImportAndSave() {
     override fun run(args: List<String>, workingDir: String?) {
+        setIndexInitialization(false)
         importProject(projectPath, jdkPath)?.let {
             setDelegationMode(projectPath, it, true)
             buildProject(it)
