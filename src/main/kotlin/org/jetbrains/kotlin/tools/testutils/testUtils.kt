6d3fd1855cff336cc2deed0dec73b1ec791db8a3
Aleksei Cherepanov <aleksei.cherepanov@jetbrains.com>
2020-08-24 10:47:29 +0300

    Fix filepath to idea sources

--- src/main/kotlin/org/jetbrains/kotlin/tools/testutils/testUtils.kt
+++ src/main/kotlin/org/jetbrains/kotlin/tools/testutils/testUtils.kt
@@ -182,7 +182,7 @@ fun setDelegationMode(path: String, project: Project, delegationMode: Boolean) {
 fun changeIdeaVersionBuild() {
     val ideaSourcesFolder = File("/mnt/cache/gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIC/202.6397.94/4fe93bb81525f2fa7a6f0fd7ba41c3b9cce9e8b6/ideaIC-202.6397.94")
     FileUtil.rename(ideaSourcesFolder, "old")
-    val newIdeaSources = File("~/ideaIC-202.6397.94")
+    val newIdeaSources = File("${System.getProperty("user.home")}/ideaIC-202.6397.94")
     if(!newIdeaSources.exists()) throw Exception("New idea sources are unavailable\n" +
             "Check https://temporary-files-cache.labs.jb.gg/cache/jps/kotlin/ideaIC/ideaIC-202.6397.94.zip\n" +
             "Or TC configuration (step 4")
