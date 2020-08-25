8a970f51387f6f4abca7c1632deed6e4f51c1b9b
Aleksei Cherepanov <aleksei.cherepanov@jetbrains.com>
2020-08-25 11:08:52 +0300

    Fix encoding

--- src/main/kotlin/org/jetbrains/kotlin/tools/testutils/testUtils.kt
+++ src/main/kotlin/org/jetbrains/kotlin/tools/testutils/testUtils.kt
@@ -179,7 +179,7 @@ fun setDelegationMode(path: String, project: Project, delegationMode: Boolean) {
 fun changeIdeaVersionBuild() {
     printMessage("=====================")
     val versionFile = File("/mnt/cache/gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIC/202.6397.94/4fe93bb81525f2fa7a6f0fd7ba41c3b9cce9e8b6/ideaIC-202.6397.94/build.txt")
-    versionFile.forEachLine(Charsets.UTF_16) { printMessage(it) }
+    versionFile.forEachLine(Charsets.UTF_8) { printMessage(it) }
     return
     val root = File("/mnt/cache/gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIC/202.6397.94/4fe93bb81525f2fa7a6f0fd7ba41c3b9cce9e8b6/")
 
